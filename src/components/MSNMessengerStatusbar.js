aa23be29778702a44bb43453c4f925f7816f0624
ManzDev <JoseRomanHernandez@gmail.com>
2021-08-24 13:06:23 +0100

    Fonts added

--- src/components/MSNMessengerStatusbar.js
+++ src/components/MSNMessengerStatusbar.js
@@ -8,11 +8,29 @@ class MSNMessengerStatusbar extends HTMLElement {
     return `
       .container {
         width: 100%;
-        height: 100%;
+        height: 14px;
         border-radius: 0 0 var(--border-radius) var(--border-radius);
         border-top: 1px solid #505E67;
         background: #FDFDFF;
         position: relative;
+        display: flex;
+        align-items: center;
+        padding-left: 8px;
+      }
+
+      .text {
+        font-family: Verdana;
+        font-size: 10px;
+        font-weight: bold;
+        color: navy;
+      }
+
+      .text a {
+        color: black;
+      }
+
+      .text a:hover {
+        color: darkred;
       }
     `;
   }
@@ -25,7 +43,7 @@ class MSNMessengerStatusbar extends HTMLElement {
     this.shadowRoot.innerHTML = `
     <style>${MSNMessengerStatusbar.styles}</style>
     <div class="container">
-
+      <div class="text">More content on <a href="https://manz.dev/">https://manz.dev/</a> and <a href="https://twitch.tv/ManzDev">Twitch</a>!</div>
     </div>`;
   }
 }
